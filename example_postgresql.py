653ac0cb34c1ef9fdd3290ac975c3672a0bed696
Philipp Jung <philipp.jung@mailbox.org>
2021-04-02 19:12:58 +0200

    Add crawler dummy container
    Use docker secrets for database authentication

--- example_postgresql.py
+++ example_postgresql.py
@@ -1,15 +1,27 @@
+import time
+
 import psycopg2
 
+from get_docker_secret import get_docker_secret
 
-def database_connection(autocommit=True):
-    conn = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        user="postgres",
-        password="example",
-    )
-    conn.autocommit = autocommit
-    return conn
+
+def database_connection(autocommit=True, retries=3, sec_between_retries=1):
+    user = get_docker_secret('postgres_user')
+    pw = get_docker_secret('postgres_pw')
+    for _ in range(3):
+        try:
+            conn = psycopg2.connect(
+                host="database",
+                port="5432",
+                user=user,
+                password=pw,
+            )
+            conn.autocommit = autocommit
+            return conn
+        except Exception:
+            time.sleep(sec_between_retries)
+
+    raise Exception("Failed to connect to database")
 
 
 def setup_database(conn_autocommit, database_name="database_name", schema_name="schema_name", table_name="table_name"):
